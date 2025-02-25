245f9c533c63ef6e79b1eb6cd7a805ddb608c63f
Linur <karts67@yandex.ru>
2025-02-25 13:13:29 +0300

    actions with numbers

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста " + text.length());
+        System.out.println("Введите первое число: ");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число: ");
+        int secondNumber = new Scanner(System.in).nextInt();
+        int sum = firstNumber + secondNumber;
+        int raznost = firstNumber - secondNumber;
+        int proizv = firstNumber * secondNumber;
+        double chastnoe = (double) firstNumber / secondNumber;
+        System.out.println("Сумма чисел: " + sum);
+        System.out.println("Разность чисел: " + raznost);
+        System.out.println("Произведение чисел: " + proizv);
+        System.out.println("Частное чисел: " + chastnoe);
     }
 }
